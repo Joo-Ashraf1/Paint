47d9fb2c3cb70e3106a14071d26fbc064b98af52
Mostafa Fathy Ali Amer <96601097+tofyfathy12@users.noreply.github.com>
2025-11-23 13:23:24 +0200

    Update color-panel.ts

--- FrontEnd/paint/src/app/color-panel/color-panel.ts
+++ FrontEnd/paint/src/app/color-panel/color-panel.ts
@@ -7,5 +7,8 @@ import { Component } from '@angular/core';
   styleUrl: './color-panel.css',
 })
 export class ColorPanel {
-
+  // currentFillColor = "white"
+  // currentStrokeColor = "black"
+  // currentStrokeWidth = 3
+  // use @Input or @Output with these variables and update them correspondigly to user selection
 }
