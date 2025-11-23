ce95822a9659fba87686f665dcb47c8cde91f9b2
Youssef Ashraf <youseefashraf54@gmail.com>
2025-11-23 19:06:44 +0200

    Updated color panel remaning is eraser

--- FrontEnd/paint/.angular/cache/20.3.11/paint/vite/deps/@angular_router.js
+++ FrontEnd/paint/.angular/cache/20.3.11/paint/vite/deps/@angular_router.js
@@ -1,12 +1,14 @@
 import {
+  Title
+} from "./chunk-XSCMQHHX.js";
+import {
   HashLocationStrategy,
   LOCATION_INITIALIZED,
   Location,
   LocationStrategy,
   PathLocationStrategy,
-  Title,
   ViewportScroller
-} from "./chunk-T4BHGBBV.js";
+} from "./chunk-U2DDV64S.js";
 import {
   APP_BOOTSTRAP_LISTENER,
   ApplicationRef,
@@ -113,7 +115,7 @@ import {
   ɵɵloadQuery,
   ɵɵqueryRefresh,
   ɵɵsanitizeUrlOrResourceUrl
-} from "./chunk-QCLXDUD2.js";
+} from "./chunk-UVGELGHZ.js";
 import {
   __async,
   __spreadProps,
--- FrontEnd/paint/src/app/color-panel/color-panel.ts
+++ FrontEnd/paint/src/app/color-panel/color-panel.ts
@@ -1,14 +1,49 @@
 import { Component } from '@angular/core';
+import {FormsModule} from '@angular/forms';
 
 @Component({
   selector: 'app-color-panel',
-  imports: [],
+  imports: [
+    FormsModule
+  ],
   templateUrl: './color-panel.html',
   styleUrl: './color-panel.css',
 })
 export class ColorPanel {
-  // currentFillColor = "white"
-  // currentStrokeColor = "black"
-  // currentStrokeWidth = 3
+   currentFillColor = "white";
+   currentStrokeColor = "black";
+   currentStrokeWidth = 3;
+   currentMode:string="fill";
+
+
+   selectColor(color: string) {
+     if(this.currentMode==='fill'){
+       this.currentFillColor=color;
+     }
+     else {
+       this.currentStrokeColor = color;
+     }
+   }
+   isActive(color: string) :boolean {
+    if(this.currentMode==='fill'){
+      if(this.currentFillColor===color){
+        return true;
+      }
+      else {
+        return false;
+      }
+    }
+    else{
+      if(this.currentStrokeColor==color){
+        return true;
+      }
+      else{
+        return false;
+      }
+    }
+
+   }
+
+
   // use @Input or @Output with these variables and update them correspondigly to user selection
 }
